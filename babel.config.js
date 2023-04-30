8df728f60246132919de0577ee692cd074b1e34d
RamonSynoro <70097613+XerlyZin@users.noreply.github.com>
2023-04-30 04:27:31 -0300

    initial commit

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@assets': './src/assets',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@storage': './src/storage',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+    ],
   };
 };
