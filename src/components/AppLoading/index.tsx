fbb9cf4e48d54d55eafbb2dafd71c2b034fb7ed1
RamonSynoro <ramon.fossa@gmail.com>
2023-08-20 01:39:32 -0300

    feat: Changing some colors from loading

--- src/components/AppLoading/index.tsx
+++ src/components/AppLoading/index.tsx
@@ -4,18 +4,18 @@ import { useTheme } from 'styled-components';
 export function AppLoading() {
 
     const theme = useTheme();
-    
+
     return (
         <Container
             style={{
-                backgroundColor: theme.COLORS.GRAY_400,
+                backgroundColor: theme.COLORS.GRAY_600,
                 flex: 1,
                 justifyContent: 'center'
-            }}
-        >
+            }}>
+                
             <ActivityIndicator 
             size={70} 
-            color={theme.COLORS.GREEN_500}
+            color={theme.COLORS.GREEN_700}
             />
 
         </Container>
