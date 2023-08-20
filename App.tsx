f46fe97599e24af207b25e7803499be4510ab45e
RamonSynoro <ramon.fossa@gmail.com>
2023-08-20 01:31:34 -0300

    feat: Implementing expoFonts and AppLoading Component until fonts are loaded

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
-import { Groups } from "@screens/Groups";
-import { StatusBar } from "react-native";
-import { ThemeProvider } from "styled-components";
-import theme from '@theme/index'
+import { Groups } from '@screens/Groups';
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import { StatusBar } from 'react-native';
+import { AppLoading } from '@components/AppLoading';
+import { ThemeProvider } from 'styled-components';
+import theme from '@theme/index';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle='light-content' backgroundColor='transparent' translucent={true}/>
-      <Groups />
+      { !fontsLoaded ? <AppLoading /> : <Groups />}
     </ThemeProvider>
   );
 }
