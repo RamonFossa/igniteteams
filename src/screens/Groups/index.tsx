8c49c782dff2c168a1ca0058bb87e8e1d9da4bdb
RamonSynoro <ramon.fossa@gmail.com>
2023-08-19 23:54:15 -0300

    refact: Implementing theme in components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,9 +3,6 @@ import * as S from './styles';
 export function Groups() {
   return (
     <S.Container>
-        <S.MainText>
-            Groups
-        </S.MainText>
     </S.Container>
   );
 }
