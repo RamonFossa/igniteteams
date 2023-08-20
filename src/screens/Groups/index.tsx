a337d341035f2d07d359ecc9983581af828ea3ce
RamonSynoro <ramon.fossa@gmail.com>
2023-08-20 03:27:38 -0300

    feat: Implementing Header and Highlight components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,15 @@
 import * as S from './styles';
+import { Header } from '@components/Header';
+import { Hightlight } from '@components/Highlight';
 
 export function Groups() {
   return (
     <S.Container>
-      
+      <Header />
+      <Hightlight 
+        title='Turmas'
+        subTitle='Jogue com a sua turma'
+      />
     </S.Container>
   );
 }
