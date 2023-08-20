d0423e71863bcbc8da7e0582a74f74ae7b332d94
RamonSynoro <ramon.fossa@gmail.com>
2023-08-19 23:52:52 -0300

    chore: Creating a Default theme and Implementing Theme provider

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { Groups } from "@screens/Groups";
 import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
+import theme from '@theme/index'
 
 export default function App() {
   return (
-    <>
-      <StatusBar barStyle='light-content' />
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent={true}/>
       <Groups />
-    </>
+    </ThemeProvider>
   );
 }
