18b6b2ed252a6f481077d40e39e02564f8e5a05d
RamonSynoro <ramon.fossa@gmail.com>
2023-09-04 00:12:29 -0300

    feat: Adding new Components on GroupsScreen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,8 +2,14 @@ import * as S from './styles';
 import { Header } from '@components/Header';
 import { Hightlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+  
   return (
     <S.Container>
       <Header />
@@ -11,7 +17,24 @@ export function Groups() {
         title='Turmas'
         subTitle='Jogue com a sua turma'
       />
-      <GroupCard title='Galera do Ignite' />
+    
+    <FlatList 
+      data={groups}
+      keyExtractor={item => item}
+      contentContainerStyle={groups.length === 0 && {flex: 1}}
+      renderItem={({item}) => (
+        <GroupCard 
+        title={item}
+        onPress={() => console.log('Clicou em', item)}
+        />
+      )}
+      ListEmptyComponent={() => (<ListEmpty message='Que tal cadastrar a primeira turma?' />)}
+    />
+    <Button 
+    title='Criar nova turma'
+    onPress={() => setGroups([... groups, `Time ${groups.length + 1}`])}
+    />
+
     </S.Container>
   );
 }
