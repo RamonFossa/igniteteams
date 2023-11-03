d3f00bd438ec1626d8aadaf4903118a271d96cbf
Ramon Fossa Chiesi <ramon.fossa@triibo.com.br>
2023-11-03 00:36:09 -0300

    feat: Adding new screen

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { Groups } from '@screens/Groups';
-import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import { NewGroup } from '@screens/NewGroup';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { StatusBar } from 'react-native';
 import { AppLoading } from '@components/AppLoading';
 import { ThemeProvider } from 'styled-components';
@@ -10,8 +11,8 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent={true}/>
-      { !fontsLoaded ? <AppLoading /> : <Groups /> }
+      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent={true} />
+      {!fontsLoaded ? <AppLoading /> : <NewGroup />}
     </ThemeProvider>
   );
 }
