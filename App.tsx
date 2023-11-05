a6050554e737dd87ef1d919ace7331c2724319ea
Ramon Fossa Chiesi <ramon.fossa@triibo.com.br>
2023-11-04 23:50:05 -0300

    feat: Adding ReactNavigation dependencie

--- App.tsx
+++ App.tsx
@@ -1,18 +1,22 @@
 import { Groups } from '@screens/Groups';
 import { NewGroup } from '@screens/NewGroup';
+import { Teams } from '@screens/Teams';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { StatusBar } from 'react-native';
 import { AppLoading } from '@components/AppLoading';
 import { ThemeProvider } from 'styled-components';
 import theme from '@theme/index';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent={true} />
-      {!fontsLoaded ? <AppLoading /> : <NewGroup />}
+      <NavigationContainer>
+        <StatusBar barStyle='light-content' backgroundColor='transparent' translucent={true} />
+        {!fontsLoaded ? <AppLoading /> : <Teams />}
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
